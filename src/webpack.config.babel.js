3e45dda8d5c82316aa456ed1f5cd2367161d299d
Thomas Piechowiak <thomas.piechowiak@nlfpaz.com>
2024-05-25 23:57:13 -0700

    'browser'

---
+++ src/webpack.config.babel.js
@@ -0,0 +1,20 @@
+import {join} from 'path'
+
+const include = join(__dirname,'src')
+
+export default {
+    entry: './src/index',
+    output:{
+        path: join(__dirname,'dist')
+        libraryTarget: 'umd',
+        library:'rforquickbase'
+    },
+    devtool: 'source-map',
+    module: {
+            loaders: [
+                {test: /\.js$/, loader: 'babel', include},
+                {test: /\.js$/, loader: 'json', include},
+            ]
+    }
+}
+
